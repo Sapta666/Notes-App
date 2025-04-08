af3814902b23d076e9e3bf26c225031d53608b0b
Saptarshi Dihidar <dihidar.saptarshi@gmail.com>
2025-04-08 05:31:37 +0530

    project restructured to add database folder

---
+++ Database/notesadmin_main.sql
@@ -0,0 +1,15 @@
+--  DB CREATION SQL
+
+-- create pluggable database notes_app 
+-- admin user notesadmin identified by notesadmin_notes_app
+-- FILE_NAME_CONVERT=('/pdbseed/'
+--                    ,'/notes_app_pdb/');
+                    
+-- ============================================================================                   
+                    
+-- For setting the database state to open , must be executed by sys
+
+-- alter pluggable database notes_app open;
+
+-- ============================================================================
+
