9583f214de7f4a874bde2f1e85830128c39f34a0
Saptarshi Dihidar <dihidar.saptarshi@gmail.com>
2025-05-15 09:00:47 +0530

    endPoint updated

--- Angular_App/src/environment/environment.ts
+++ Angular_App/src/environment/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
-    endPoint: "http://localhost:8080/notes-app-0.0.1-SNAPSHOT",
+    // endPoint: "http://localhost:8080/notes-app-0.0.1-SNAPSHOT",
+    
+    endPoint: "http://100.27.28.77:8080/notes-app-0.0.1-SNAPSHOT",    
 
     cryptoKey: "1996052222051996",
 }
